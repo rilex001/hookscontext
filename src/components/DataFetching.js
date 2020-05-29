30ef32d43951d33fc0e4f0d63a383a490285e0b3
Nikola <ristic1900@gmail.com>
2020-05-29 22:26:53 +0200

    add button

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,28 +2,36 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const  DataFetching = () => {
-    const [posts, setPosts] = useState([])
-
+    const [post, setPost] = useState({})
+    const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
         .then(res => {
             console.log(res)
-            setPosts(res.data)
+            setPost(res.data)
         })
         .catch(err => {
             console.log(err)
         })
-    }, [])
+    }, [id])
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
 
     return (
         <div>
-            <ul>
+            <input type='text' value={id} onChange={e => setId(e.target.value)}/>
+            <button type='button' onClick={handleClick} >Enter a number</button>
+            <div>{post.title}</div>
+            {/* <ul>
                 {
                    posts.map( post => (
                        <li key={post.id}>{post.title}</li>
                    )) 
                 }
-            </ul>
+            </ul> */}
         </div>
     )
 }
